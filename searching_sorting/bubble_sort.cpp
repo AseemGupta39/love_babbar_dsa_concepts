b2bbdbdae009ef540a89050938c0a4975b066c75
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-10-01 18:55:28 +0530

    fixed error in bubble sort

--- searching_sorting/bubble_sort.cpp
+++ searching_sorting/bubble_sort.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-void bubbleSort(vector<int>& arr, int n)
+using std::swap;
+using std::vector;
+using std::cin;
+using std::cout;
+
+void print_vec(vector<int>& arr){
+    for(const int &i:arr){
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+
+
+void bubbleSort(vector<int>& arr, int &n)
 {   
     int i,j;
     for(i=1;i<n;i++){
@@ -14,11 +26,13 @@ void bubbleSort(vector<int>& arr, int n)
     }
 }
 
-void optimized_bubbleSort(vector<int>& arr, int n)
+
+void optimized_bubbleSort(vector<int>& arr, int &n)
 {   
     int i,j;
     bool swapped;
     for(i=1;i<n;i++){
+        print_vec(arr);
         swapped = false;
         for(j=0;j<n-i;j++){
             if(arr[j] > arr[j+1]){
@@ -27,7 +41,7 @@ void optimized_bubbleSort(vector<int>& arr, int n)
             }
         }
         if(swapped == false){
-            break;
+            return;
         }
     }
 }
@@ -36,6 +50,24 @@ void optimized_bubbleSort(vector<int>& arr, int n)
 int main() {
   cout << "program started\n";
 
+    int n ;
+    cin >> n;
+
+    vector<int> inp(n);
+
+    for(int &i:inp){
+        cin >> i;
+    }
+
+    optimized_bubbleSort(inp,n);
+
+    for(const int &i:inp){
+        cout << i << " ";
+    }
+
   cout << "\nprogram ended\n";
   return 0;
 }
+
+// 5
+// 97 65 32 21 9
