e4843e75b6fa63b3e616428c64d39dd01866f16d
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-10-08 01:11:45 +0530

    implemented stack using linked list

--- stack/implementation.cpp
+++ stack/implementation.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-using namespace std;
+#include <list>
+
+
+using std::list;
+using std::cout;
+using std::cin;
 
 class Stack{
 
@@ -39,37 +44,80 @@ class Stack{
         return arr[top];
     }
 
-    inline bool if_empty(){
+    inline bool is_empty(){
         return top==-1;
     }
     
 };
 
 
+class Stack_l{
+
+    public:
+        // int top;
+        list<int> l;
+
+
+    void push(int element){
+       l.push_back(element);
+    }
+
+    void pop(){
+        if(l.empty()){
+            cout << "already empty\n";
+        }
+        else{
+            l.pop_back();
+        }
+    }
+
+    int peek(){
+        if(l.empty()){
+            cout << "list empty\n";
+            return -1;
+        }
+        return l.back();
+    }
+
+    inline bool is_empty(){
+        return l.empty();
+    }
+    
+};
+
 int main() {
   cout << "program started\n";
 
     
-  Stack st(5);
-    st.push(20);
-    cout << st.peek() << " ";
-    st.push(70);
-    cout << st.peek() << " ";
-    st.push(40);
-    cout << st.peek() << " ";
-    st.push(64);
-    cout << st.peek() << " ";
-    st.push(80);
-    cout << st.peek() << " ";
-    st.push(98);
-    cout << st.peek() << " ";
+//   Stack st(5);
+//     st.push(20);
+//     cout << st.peek() << " ";
+//     st.push(70);
+//     cout << st.peek() << " ";
+//     st.push(40);
+//     cout << st.peek() << " ";
+//     st.push(64);
+//     cout << st.peek() << " ";
+//     st.push(80);
+//     cout << st.peek() << " ";
+//     st.push(98);
+//     cout << st.peek() << " ";
 
-    cout << "\n\n";
-    while(!st.if_empty()){
-        cout << st.peek() << " ";
-        st.pop();
-    }
+//     cout << "\n\n";
+//     while(!st.is_empty()){
+//         cout << st.peek() << " ";
+//         st.pop();
+//     }
     
+    // Stack_l st;
+    // st.push(15);
+    // st.push(25);
+    // st.push(35);
+    // st.push(45);
+    // while(!st.is_empty()){
+    //     cout << st.peek() << " ";
+    //     st.pop();
+    // }
     
     
   cout << "\nprogram ended\n";
