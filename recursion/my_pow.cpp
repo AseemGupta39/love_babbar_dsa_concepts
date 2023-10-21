b21a4eb0391967a232cdbb000cfba1002a0c77c4
AseemGupta39 <aseemguptajaipur@gmail.com>
2023-10-21 18:02:10 +0000

    power of 2 updated

--- recursion/my_pow.cpp
+++ recursion/my_pow.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+
 
 double my_pow(double base,int exp){
     if(exp==0){
@@ -17,14 +19,35 @@ double my_pow(double base,int exp){
     return my_pow(base*base,exp>>1);  
 }
 
+unsigned long long sq_num(long long &n){
+    if(n==0){
+        return 0;
+    }
+
+    if(n<0){
+        n = -n;
+    }
+
+    long long x = n >> 1;
+
+    return (n & 1) ? ((sq_num(x) << 2) + (x << 2) + 1):(sq_num(x) << 2);
+}
+
 int main() {
   cout << "program started\n";
 
-  long long base,exp;
-    cin >> base >> exp;
+  // long long base,exp;
+    // cin >> base >> exp;
 
-    cout << my_pow(base,exp);
+    // cout << my_pow(base,exp);
 
+    long long n;
+    cin >> n;
+    
+    // while(n--)
+    printf("%llu\n",sq_num(n));
+    // cout << sq_num(n) << "\n";
+    
   cout << "\nprogram ended\n";
   return 0;
 }
